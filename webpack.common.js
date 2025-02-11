98cfc8c77c1e94655301a1562d5cd9a35654a731
Andrew Perotti <Perotti28@gmail.com>
2025-02-11 13:02:07 -0800

    Add webpack-merge, scripts, config files
    Webpack-merge allows us to use slightly different config files for webpack depending on if we're going to run webpack with our code for development purposes or production purposes/testing.
    Scripts allow us to call on these separate configurations of our webpack.
    And as already explained somewhat via what webpack-merge does we now have multiple webpack versions and in these files the differences are stated and we merge in the common settings as well.

---
+++ webpack.common.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    plugins: [
+
+    ],
+    output: {
+        filename: `[name].bundle.js`,
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    }
+}
