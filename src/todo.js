f35253d96cf7e999e6eead85d32086f4e2d35003
Andrew Perotti <Perotti28@gmail.com>
2025-02-13 11:15:13 -0800

    Add identifier to constructor, methods to class
    Again the identifier will help us to target specific todo objects in different use cases.
    Methods were added to do various actions, mainly using identifier property, which again I'll link later to the visuals using the dataset attribute.

--- src/todo.js
+++ src/todo.js
@@ -1,20 +1,38 @@
 import Checklist from './checklist.js'
 
 export default class{
-    constructor(name, dueDate, priority, description, notes=[], checklist=[]){
+    constructor(name, dueDate, priority, description, notes=[], checklist=[], identifier = parent.length++){
         this.name = name;
-        this.dueDate = dueDate;
+        this.dueDate = Date(dueDate);
         this.priority = priority;
         this.description = description;
         this.notes = notes;
         this.checklist = checklist
         this.completed = false
+        this.identifier = identifier
     }
 
-    //delete todo
-    //complete todo
-    //change todo property value (this is not an array)
-    //create checklist item
+    //complete todo toggle
+    toggleTodoComplete(){
+        this.completed = this.completed===true ? false:true;
+    }
+    //change todo property value (not an arrays)
+    changeNonArrayProps(prop, val){
+        this.prop = val
+    }
+
+    //create checklist item, by calling constructor and pushing return object into array of checklist property
+    createChecklistItem(text){
+        this.checklist.push(new Checklist(text))
+    }
     //delete checklist item
+    deleteChecklistItem(checkIdentifier){
+        let identifierIndex = this.checklist.indexOf({identifier: checkIdentifier})
+        this.checklist.splice(identifierIndex, identifierIndex)
+    }
     //delete note item
+    deleteNoteItem(noteValue){
+        let noteIndex = this.notes.indexOf(notevalue)
+        this.notes.splice(noteIndex, noteIndex)
+    }
 }
