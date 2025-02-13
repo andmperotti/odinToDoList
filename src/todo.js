59d684d3cc6c7bed58482999991713efd74f646c
Andrew Perotti <Perotti28@gmail.com>
2025-02-13 09:32:33 -0800

    Add class for todo items
    For now we have a class to create todo items and their properties, next I'll add the methods that required the separation from the Project class.

---
+++ src/todo.js
@@ -0,0 +1,12 @@
+export default class{
+    constructor(name, dueDate, priority, description, notes=[], checklist=[]){
+        this.name = name;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.description = description;
+        this.notes = notes;
+        this.checklist = checklist
+    }
+
+    
+}
