2849032da25fac78ded41f5ca2aea1fd158725bd
Andrew Perotti <Perotti28@gmail.com>
2025-02-13 09:26:36 -0800

    Add a createTodo method to Project class
    This allows us to create todo objects which will be saved into projects todos array, we want todos to be an object because they can hold a lot of information but we still want them tied to a specific project.

--- src/project.js
+++ src/project.js
@@ -1,7 +1,13 @@
 export default class{
-    constructor(name, description){
+    constructor(name, description, identifier=0){
         this.name = name;
         this.description = description
         this.todos = []
+        this.identifier = identifier
     }
+
+    createTodo(name, dueDate, priority, description, notes=[], checklist=[]){
+        this.todos.push({name, dueDate, priority, description, notes, checklist})
+    }
+    
 }
