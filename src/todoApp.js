557874baf14fd52921071b3b72e4bf6e44c05b53
Andrew Perotti <Perotti28@gmail.com>
2025-02-13 11:22:28 -0800

    Add functions to add and delete Projects
    This module handles just the creation and deletion of project objects.

---
+++ src/todoApp.js
@@ -0,0 +1,20 @@
+import Project from './project.js'
+
+let projects = localStorage.getItem('projectArray') || []
+
+function addProject(name, description){
+    projects.push(new Project(name, description))
+    localStorage.setItem('projectsArray', JSON.parse(projects))
+}
+
+function deleteProject(id){
+    let projectIndex = projects.indexOf({identifier: id})
+    projects.splice(projectIndex, projectIndex)
+    localStorage.setItem('projectsArray', JSON.stringify(projects))
+}
+
+
+
+
+
+
