1d4f5fa713ca0f0adfbb26d9aba39c69ce90b05e
Andrew Perotti <Perotti28@gmail.com>
2025-02-13 11:27:14 -0800

    Change property accessor
    So I'm not sure if I'm going to be able to correctly set an identifier by using a parent object but I'll try.

--- src/checklist.js
+++ src/checklist.js
@@ -1,5 +1,5 @@
 export default class{
-    constructor(text, checked=false, identifier=parent.checklist.length++){
+    constructor(text, checked=false, identifier=parent.length++){
         this.text = text;
         this.checked = false;
         this.identifier = identifier
