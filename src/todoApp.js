987c50bee3a18c5013145c203293cd8e62e74a0e
Andrew Perotti <Perotti28@gmail.com>
2025-02-13 13:35:34 -0800

    Change getProjects, add starting projectsArray,etc
    First off we start this iife module pattern by setting an array as value to projectsArray if it did not hold an array prior, which will let us just add projects thereafter without worrying about the array.
    Secondly I changed the getProjects function to pull the currently saved value of projectsArray out and convert it to js code from json string, then depending on how many projects are in that array we either produce a semi neat output or we tell the user there are no projects saved at this time.
    I also changed the addProject and deleteProject to correctly parse the JSON data pulled from projectsArray however I'm trying to use an identifier to delete projects but that property is not being set up correctly so will have to look at the project.js further to fix that.

--- src/todoApp.js
+++ src/todoApp.js
@@ -1,19 +1,33 @@
 import Project from './project.js'
 
 const todoApp = (()=>{
-    let projects = localStorage.getItem('projectArray') || []
+    if(!localStorage.getItem("projectsArray")){
+        localStorage.setItem("projectsArray", '[]')
+    }
+    function getProjects(){
+        let projects = JSON.parse(localStorage.getItem("projectsArray"))
+        if(projects.length>0){
+            for(let project of projects){
+                console.log(project)
+            }
+        }else if(projects.length<1){
+            console.log('No projects stored thus far')
+        }
+    }
 
     function addProject(name, description){
+        let projects = JSON.parse(localStorage.getItem("projectsArray"))
         projects.push(new Project(name, description))
         localStorage.setItem('projectsArray', JSON.stringify(projects))
     }
 
     function deleteProject(id){
+        let projects = JSON.parse(localStorage.getItem("projectsArray"))
         let projectIndex = projects.indexOf({identifier: id})
         projects.splice(projectIndex, projectIndex)
         localStorage.setItem('projectsArray', JSON.stringify(projects))
     }
-    return {projects, addProject, deleteProject}
+    return {getProjects, addProject, deleteProject}
 })()
 
 export { todoApp }
