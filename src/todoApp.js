a3a278ed9fcc4e7ac4107f07caf093e95eda6ae4
Andrew Perotti <Perotti28@gmail.com>
2025-02-13 11:25:06 -0800

    Change JSON method being used in addProject
    I previously had JSON.parse which turns js into

--- src/todoApp.js
+++ src/todoApp.js
@@ -4,7 +4,7 @@ let projects = localStorage.getItem('projectArray') || []
 
 function addProject(name, description){
     projects.push(new Project(name, description))
-    localStorage.setItem('projectsArray', JSON.parse(projects))
+    localStorage.setItem('projectsArray', JSON.stringify(projects))
 }
 
 function deleteProject(id){
