86803e18008e86ae9eb3bf6530f3eddf8bbc7595
Andrew Perotti <Perotti28@gmail.com>
2025-02-13 11:18:45 -0800

    Remove comments
    As previously stated, because I used more descriptive arguments to the methods we now don't need the comments to explain what's going on.

--- src/todo.js
+++ src/todo.js
@@ -11,26 +11,19 @@ export default class{
         this.completed = false
         this.identifier = identifier
     }
-
-    //complete todo toggle
     toggleTodoComplete(){
         this.completed = this.completed===true ? false:true;
     }
-    //change todo property value (not an arrays)
     changeNonArrayProps(todoProp, newVal){
         this.todoProp = newVal
     }
-
-    //create checklist item, by calling constructor and pushing return object into array of checklist property
     createChecklistItem(descriptionText){
         this.checklist.push(new Checklist(descriptionText))
     }
-    //delete checklist item
     deleteChecklistItem(checklistIdentifier){
         let identifierIndex = this.checklist.indexOf({identifier: checklistIdentifier})
         this.checklist.splice(identifierIndex, identifierIndex)
     }
-    //delete note item
     deleteNoteItem(noteElementString){
         let noteIndex = this.notes.indexOf(noteElementString)
         this.notes.splice(noteIndex, noteIndex)
