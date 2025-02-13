48de01be5e91930a0669ff359d49dcdc52e57d48
Andrew Perotti <Perotti28@gmail.com>
2025-02-13 13:03:37 -0800

    Wrap variable and functions inside an iife
    This allows us to build an object and export that object to the user side area.

--- src/todoApp.js
+++ src/todoApp.js
@@ -1,20 +1,22 @@
 import Project from './project.js'
 
-let projects = localStorage.getItem('projectArray') || []
+const todoApp = (()=>{
+    let projects = localStorage.getItem('projectArray') || []
 
-function addProject(name, description){
-    projects.push(new Project(name, description))
-    localStorage.setItem('projectsArray', JSON.stringify(projects))
-}
+    function addProject(name, description){
+        projects.push(new Project(name, description))
+        localStorage.setItem('projectsArray', JSON.stringify(projects))
+    }
 
-function deleteProject(id){
-    let projectIndex = projects.indexOf({identifier: id})
-    projects.splice(projectIndex, projectIndex)
-    localStorage.setItem('projectsArray', JSON.stringify(projects))
-}
+    function deleteProject(id){
+        let projectIndex = projects.indexOf({identifier: id})
+        projects.splice(projectIndex, projectIndex)
+        localStorage.setItem('projectsArray', JSON.stringify(projects))
+    }
+    return {projects, addProject, deleteProject}
+})()
 
-
-
+export { todoApp }
 
 
 
