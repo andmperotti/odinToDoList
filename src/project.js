b5a01f3239f091f68626df8b69da24bac9edc456
Andrew Perotti <Perotti28@gmail.com>
2025-02-14 09:19:33 -0800

    Add logTodos method
    This method when called will show the user in the console all of the todo items and their properties, so more or less the summary of what a todo ui element will be.

--- src/project.js
+++ src/project.js
@@ -12,4 +12,11 @@ export default class{
     changePrimitivePropVal(todoProp, newPropVal){
         this.todoProp = newValue
     }
+    logTodos(){
+        if(this.todos.length>0){
+            for(let todo of this.todos){
+                console.log(`Todo name: ${todo.name}, todo due date: ${todo.dueDate}, todo priority: ${todo.priority}, todo description: ${todo.description}, todo notes: ${todo.notes.join(', ')}, todo checklist: ${todo.checklist.join(', ')}, todo completion: ${todo.completed}`)
+            }
+        }
+    }
 }
