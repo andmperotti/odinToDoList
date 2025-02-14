9cf096ce04561dbcadeb6d5f3e5381c15192c524
Andrew Perotti <Perotti28@gmail.com>
2025-02-14 09:04:01 -0800

    Remove identifier prop, change method and arg name
    Once again the identifier value acquisition was not working as intended and have reverted to using index positions.
    I've also changed the name of the method that allows us to change a property of a Project to a different value and changed its argument name to versions less verbose and more to the point.

--- src/project.js
+++ src/project.js
@@ -1,16 +1,15 @@
 import Todo from './todo.js'
 
 export default class{
-    constructor(name, description, identifier=0){
-        this.name = name;
+    constructor(name, description){
+        this.name = name
         this.description = description
         this.todos = []
-        this.identifier = identifier
     }
-    deleteTodo(todoIdentifier){
-        let todoIndex = this.todos.indexOf({identifier:todoIdentifier})
+    deleteTodo(todoIndex){
+        this.todos.splice(todoIndex, todoIndex)
     }
-    changeNonArrayProjectProp(todoProp, newValue){
+    changePrimitivePropVal(todoProp, newPropVal){
         this.todoProp = newValue
     }
 }
