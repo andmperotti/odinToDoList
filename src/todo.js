a404f8da20f7601bb85e4a9f8c710554a72cbe7d
Andrew Perotti <Perotti28@gmail.com>
2025-02-14 08:54:04 -0800

    Remove identifier property, rename arguments, etc
    As the logic for setting an identifier value was off, but not sure if impossible, I've defaulted to using other values, like index and string property values.
    I've also renamed arguments to help make functions actions clearer.
    I also think I should just use index positions rather than anything else so may make more changes after this commit.

--- src/todo.js
+++ src/todo.js
@@ -1,28 +1,27 @@
 import Checklist from './checklist.js'
 
 export default class{
-    constructor(name, dueDate, priority, description, notes=[], checklist=[], identifier = parent.length++){
-        this.name = name;
-        this.dueDate = Date(dueDate);
-        this.priority = priority;
-        this.description = description;
-        this.notes = notes;
+    constructor(name, dueDate, priority, description, notes=[], checklist=[]){
+        this.name = name
+        this.dueDate = Date(dueDate)
+        this.priority = priority
+        this.description = description
+        this.notes = notes
         this.checklist = checklist
         this.completed = false
-        this.identifier = identifier
     }
     toggleTodoComplete(){
         this.completed = this.completed===true ? false:true;
     }
-    changeNonArrayProps(todoProp, newVal){
-        this.todoProp = newVal
+    changeNonArrayPropValue(todoProp, newPropVal){
+        this.todoProp = newPropVal
     }
-    createChecklistItem(descriptionText){
-        this.checklist.push(new Checklist(descriptionText))
+    createChecklistItem(checklistItemText){
+        this.checklist.push(new Checklist(checklistItemText))
     }
-    deleteChecklistItem(checklistIdentifier){
-        let identifierIndex = this.checklist.indexOf({identifier: checklistIdentifier})
-        this.checklist.splice(identifierIndex, identifierIndex)
+    deleteChecklistItem(checklistItemText){
+        let checklistItemIndex = this.checklist.findIndex(checkItem=>checkItem.text===checklistItemText)
+        this.checklist.splice(checklistItemText, checklistItemText)
     }
     deleteNoteItem(noteElementString){
         let noteIndex = this.notes.indexOf(noteElementString)
