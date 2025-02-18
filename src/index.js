97dd8e60c3a180ced31c95b24e07578d3aaee9d8
Andrew Perotti <Perotti28@gmail.com>
2025-02-18 13:30:09 -0800

    Delete everything and start over
    Sadly I have gotten lost and am going back to planning out using pseudo code before I write any code, and will attempt considering all possible actions and properties/etc.

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import { todoApp } from './todoApp.js'
-
-//link the imported module to the window object thus allowing us to call from todoApp while our users are interacting inside the browsers window.
-window.todoApp = todoApp
