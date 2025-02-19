c61dc13e898137edf2fb5dce9f3aaf1a015c30ed
Andrew Perotti <perotti28@gmail.com>
2025-02-18 16:52:00 -0800

    Add starting files, and pseudocode
    template.html and index.js are required otherwise webpack won't serve.
    todoApp.js holds our apps pseudocode, I'm just planning on writing all application logic in one file since my previous attempts at multiple modules ran into circular dependency issues.

---
+++ src/index.js
@@ -0,0 +1,2 @@
+//Import the application logic module/etc, if done with creating that then import the UI module
+
---
+++ src/todoApp.js
@@ -0,0 +1,62 @@
+//create classes for projects, todo items, and checklist items, and storage functions to save a pojo local version of the app data to localStorage, which we can run functions on which then save those changes to the local storage.
+
+//project class
+    //name and description properties
+    //todos property which is an array which will hold Todo class instances
+
+//todo class
+    //title, description, dueDate, priority, notes, and checklist
+        //notes will be an array which just holds strings
+        //checklist will be an array which holds instances of Checklist class instances
+
+//checklist class
+    //description, and a boolean value property of 'checked'
+
+
+
+//createProject function
+
+//deleteProject function
+
+
+
+
+//create todo function
+
+//complete todo function
+
+//delete todo function
+
+//add note to todo function
+
+//delete not from todo function
+
+
+
+
+//create checklist function 
+
+//toggle checklist item function
+
+//delete checklist item
+
+
+
+//save to localStorage function
+
+//load from local Storage function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//change instance property value function; can be used to change the value of any property that is not holding an array
