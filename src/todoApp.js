d153bf24137903ef6d5741c314050a9793f33e7b
Andrew Perotti <perotti28@gmail.com>
2025-02-18 17:17:17 -0800

    Move pseudocode to better locations, add props
    Moved pseudocode into class declarations as I think the functions will only be used in those locations, and kept more global or wide reaching functions outside of class declarations.
    I also added the properties for class constructors, some using default values to create arrays.

--- src/todoApp.js
+++ src/todoApp.js
@@ -3,49 +3,76 @@
 //project class
     //name and description properties
     //todos property which is an array which will hold Todo class instances
+class Project{
+    constructor(title, description, todos=[]){
+        this.title = title
+        this.description = description
+        this.todos = todos
+    }
+    //create todo function
+
+    //complete todo function
+
+    //delete todo function
+}
 
 //todo class
     //title, description, dueDate, priority, notes, and checklist
         //notes will be an array which just holds strings
         //checklist will be an array which holds instances of Checklist class instances
+class Todo{
+    constructor(name, description, priority, dueDate, notes=[], checklist=[]){
+        this.name = name
+        this.description = description
+        this.priority = priority
+        this.dueDate = Date(dueDate)
+        this.notes = notes
+        this.checklist = checklist
+    }
+    //add note to todo function
+
+    //delete not from todo function
+
+    //create checklist function 
+
+    //toggle checklist item function
+
+    //delete checklist item
+}
 
 //checklist class
     //description, and a boolean value property of 'checked'
-
+class Checklist{
+    constructor(description, checked=false){
+        this.description = description
+        this.checked = checked
+    }
+}
 
 
 //createProject function
+function createProject(){
+
+}
 
 //deleteProject function
+function deleteProject(){
 
-
-
-
-//create todo function
-
-//complete todo function
-
-//delete todo function
-
-//add note to todo function
-
-//delete not from todo function
-
-
-
-
-//create checklist function 
-
-//toggle checklist item function
-
-//delete checklist item
+}
 
 
 
 //save to localStorage function
+function saveToStorage(data){
+
+}
 
 //load from local Storage function
-
+function loadStorage(){
+    
+}
+//create local variable which is the converted local storage into JSON
+let projects = loadStorage()
 
 
 
