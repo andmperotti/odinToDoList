24012b31330872856c2a792ef208371bc764098d
Andrew Perotti <Perotti28@gmail.com>
2025-02-24 10:28:16 -0800

    Add functionality for todoApp
    I recreated the todoApp module, which is an iife factory function which by invoking allows us to export its functions and variables to one location our index.js.
    I was able to not use a replacer or reviver in my JSON functions when saving and loading data to and from localStorage.
    My biggest snag was in the loadStorage function which not only parses JSON data from localStorage and converts it into not only javascript code but into new instances of classes, but when I went to overwrite the projects variable I was mistakenly passing returnedProjects in reference, and not actually making copies, and this was fixed by using a for of loop and pushing the objects in.

--- src/todoApp.js
+++ src/todoApp.js
@@ -1,127 +1,113 @@
-//create classes for projects, todo items, and checklist items, and storage functions to save a pojo local version of the app data to localStorage, which we can run functions on which then save those changes to the local storage.
-
-//project class
-    //name and description properties
-    //todos property which is an array which will hold Todo class instances
-class Project{
-    constructor(title, description, todos=[]){
-        this.title = title
-        this.description = description
-        this.todos = todos
-    }
-    //create todo function
-    createTodo(todoName, todoDescription, todoPriority, todoDueDate, todoNotes, todoChecklist){
-        this.todos.push(new Todo(todoName, todoDescription, todoPriority, todoDueDate, todoNotes, todoChecklist))
-    }
-
-    //complete todo function
-    completeTodo(todoIndex){
-        this.todos[todoIndex].complete=true
-    }
-    //delete todo function
-    deleteTodo(todoIndex){
-        this.todos.splice(todoIndex, 1)
-    }
-}
-
-//todo class
-    //title, description, dueDate, priority, notes, and checklist
-        //notes will be an array which just holds strings
-        //checklist will be an array which holds instances of Checklist class instances
-class Todo{
-    constructor(name, description, priority, dueDate, notes=[], checklist=[], complete=false){
-        this.name = name
-        this.description = description
-        this.priority = priority
-        this.dueDate = Date(dueDate)
-        this.notes = notes
-        this.checklist = checklist
-        this.complete = complete
-    }
-    //add note to todo function
-    addNote(noteText){
-        this.notes.push(noteText)
-    }
-    //delete note from todo function
-    deleteNote(noteIndex){
-        this.notes.splice(noteIndex, 1)
-    }
-    //create checklist function 
-    createChecklistItem(checkDescription){
-        this.checklist.push(new Checklist(checkDescription))
-    }
-    //toggle checklist item function
-    toggleChecklistItem(checklistIndex){
-        if(this.checklist[checklistIndex]===true){
-            this.checklist[checklistIndex]=false
-        }else{
-            this.checklist[checklistIndex]=true
+const todoApp = (function(){
+    class Project{
+        constructor(title, description, todos=[]){
+            this.title = title
+            this.description = description
+            this.todos = todos
+        }
+        createTodo(todoName, todoDescription, todoPriority, todoDueDate, todoNotes, todoChecklist){
+            this.todos.push(new Todo(todoName, todoDescription, todoPriority, todoDueDate, todoNotes, todoChecklist))
+            console.log(`${todoName} created!`)
+        }
+        completeTodo(todoIndex){
+            this.todos[todoIndex].complete=true
+        }
+        deleteTodo(todoIndex){
+            this.todos.splice(todoIndex, 1)
         }
     }
-    //delete checklist item
-    deleteChecklistItem(checklistIndex){
-        this.checklist.splice(checklistIndex, 1)
+
+    class Todo{
+        constructor(name, description, priority, dueDate, notes=[], checklist=[], complete=false){
+            this.name = name
+            this.description = description
+            this.priority = priority
+            this.dueDate = Date(dueDate)
+            this.notes = notes
+            this.checklist = checklist
+            this.complete = complete
+        }
+        addNote(noteText){
+            this.notes.push(noteText)
+        }
+        deleteNote(noteIndex){
+            this.notes.splice(noteIndex, 1)
+        }
+        createChecklistItem(checkDescription){
+            this.checklist.push(new Checklist(checkDescription))
+        }
+        deleteChecklistItem(checklistIndex){
+            this.checklist.splice(checklistIndex, 1)
+        }
     }
-}
 
-//checklist class
-    //description, and a boolean value property of 'checked'
-class Checklist{
-    constructor(description, checked=false){
-        this.description = description
-        this.checked = checked
+    class Checklist{
+        constructor(description, checked=false){
+            this.description = description
+            this.checked = checked
+        }
+        toggleChecklistItem(checklistIndex){
+            if(this.checklist[checklistIndex].checked===true){
+                this.checklist[checklistIndex].checked=false
+            }else{
+                this.checklist[checklistIndex].checked=true
+            }
+        }
     }
-}
 
+    let projects = []
+    loadStorage()
 
-//createProject function
-function createProject(projectTitle, projectDescription){
-    projects.push(new Project())
-}
+    function createProject(projectTitle, projectDescription){
+        projects.push(new Project(projectTitle, projectDescription))
+        console.log(`${projectTitle} with a description of ${projectDescription} has been created!`)
+    }
 
-//deleteProject function
-function deleteProject(projectIndex){
-    projects.splice(projectIndex, 1)
-}
+    function deleteProject(projectIndex){
+        projects.splice(projectIndex, 1)
+    }
 
+    function saveToStorage(){
+        localStorage.setItem('projectsArray', JSON.stringify(projects))
+        console.log('Saved projects data to localStorage')
+    }
 
-
-//save to localStorage function
-    //this will need to be called anytime a change is made
-function saveToStorage(data){
-    localStorage.setItem('projectsArray', JSON.stringify(projects))
-}
-
-//load from local Storage function
-    //I think this would pretty much only be called at the initial load into a new window
-    //if data exists on localStorage then load it, otherwise save an empty array as its value
-function loadStorage(){
-    let returnProjects;
-    if(!localStorage.getItem('projectsArray')){
+    //this needs a warning, if projects contains elements not in the localStorage then loadStorage will overwrite them, maybe a yes/no question, and suggest to saveToStorage first, or something?
+    function loadStorage(){
+        let returnedProjects = [];
+        if(!localStorage.getItem('projectsArray')){
+            localStorage.setItem('projectsArray', '[]')
+        }else{
+            let parsedProjects = JSON.parse(localStorage.getItem('projectsArray'))
+            for(let project of parsedProjects){
+                returnedProjects.push(new Project(project.title, project.description, project.todos))
+            }
+            returnedProjects.forEach(project=>{
+                project.todos.forEach(todo=>{
+                    let tempTodo = todo
+                    todo = new Todo(tempTodo.name, tempTodo.description, tempTodo.priority, tempTodo.dueDate, tempTodo.notes, tempTodo.checklist)
+                    todo.checklist.forEach(checkItem=>{
+                        let tempCheckItem = checkItem
+                        checkItem = new Checklist(tempCheckItem.description, tempCheckItem.checked)
+                    })
+                })
+            })
+        }
+        //wipe projects
+        projects.splice(0, projects.length)
+        //add each converted project into projects array variable
+        for(let convertedProject of returnedProjects){
+            projects.push(convertedProject)
+        }
+        console.log('Storage loaded into projects variable from localStorage')
+    }
+    
+    function startANew(){
         localStorage.setItem('projectsArray', '[]')
-        returnProjects = []
-    }else{
-        let pojoProjects = JSON.parse(localStorage.getItem('projectsArray'))
-        returnProjects = []
-        //here use nested for loops to build Projects, whom have Todos, whom have Checklists
-        
+        projects = []
     }
-    return returnProjects
-}
 
-//create local variable which is the converted local storage from JSON
-let projects = loadStorage()
+    return {projects, Project, Todo, Checklist, loadStorage, saveToStorage, deleteProject, createProject, startANew}
+})()
 
-
-
-
-
-
-
-
-
-
-
-
-//change instance property value function; can be used to change the value of any property that is not holding an array
-export {projects, loadStorage, saveToStorage, createProject, deleteProject, Checklist, Project, Todo}
+export {todoApp}
