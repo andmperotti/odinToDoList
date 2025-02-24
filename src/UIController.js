823504b29eb6380da2539eba9397ae4a80c4b3e0
Andrew Perotti <Perotti28@gmail.com>
2025-02-24 12:46:09 -0800

    Create UIController file to hold UI functions
    This file imports todoApp into it and calls on its functions when the user interacts with the interface created by this file including listeners on buttons to create and delete projects/todos/notes/etc.

---
+++ src/UIController.js
@@ -0,0 +1,14 @@
+import { todoApp } from './todoApp.js'
+
+export  const UIController =(function(){
+    //Generate site layout elements
+        //sidebar with projects in a list (could sort these later with uncompleted task count vs due dates/etc), including a button where a new project can be made
+        //main content area where projects are opened up and todos can be seen and made. If no projects exist maybe generate an example project or explanation text that tells a user how to create a new project
+
+    //generate projects that are saved (function)
+
+    //generate project when user creates it (same function as above just called at different time (listener on a button))
+
+    //function that adds a checklist or note item into a todo and then redraws or adds that checklist or note item as an element
+    
+})()
