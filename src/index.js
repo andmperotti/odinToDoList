e08ad5d3fa9065e80da5261507ef3ac29c12cd34
Andrew Perotti <Perotti28@gmail.com>
2025-02-24 10:31:51 -0800

    Add todoApp into index.js and affiliate it
    I believe this was the same code this file held before, but well for transparency sakes I rewrote it again.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-//Import the application logic module/etc, if done with creating that then import the UI module
-import {projects, loadStorage, saveToStorage, createProject, deleteProject, Checklist, Project, Todo} from './todoApp.js'
+import { todoApp } from './todoApp.js'
 
+window.todoApp = todoApp
