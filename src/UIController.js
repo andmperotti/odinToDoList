cb65f9092f6a1cb3dd8e449f24f591118ca9e86b
Andrew Perotti <Perotti28@gmail.com>
2025-02-25 10:47:03 -0800

    Add more pseudocode
    As I looked at the todoApp.js functions I remembered that I need to have ways for the user to invoke all of those functions, and there may be more functions as well.

--- src/UIController.js
+++ src/UIController.js
@@ -3,15 +3,15 @@ import { todoApp } from './todoApp.js'
 export const UIController =(function(){
     //Generate site layout elements
         //sidebar with projects in a list (could sort these later with uncompleted task count vs due dates/etc), including a button where a new project can be made
-        //main content area where projects are opened up and todos can be seen and made. If no projects exist maybe generate an example project or explanation text that tells a user how to create a new project
+        //main content area where projects are opened up and todos can be seen and made which will be crossed out if completed, not crossed out if un complete, as well as a button to delete a todo item for any reason. If no projects exist maybe generate an example project or explanation text that tells a user how to create a new project
 
-    //I forgot I have the html template file so I can create the starting elements there instead
+    //generate projects that are saved (function), this would be in the main section, within the notes and checklist ul's create buttons to add notes and checklist items, and within those notes and checklist items create buttons to delete those items. These checklist items will also be able to have a cross through them or not depending on if they have been completed.
 
-    //generate projects that are saved (function)
+    //generate projects as a list item in the nav's ul, and give them buttons inside their list items that allows a user to delete the projects, of course prompt with a modal if they are sure they want to delete the project.
 
-    //generate project when user creates it (same function as above just called at different time (listener on a button))
+    //generate project when user creates it (same function as above just called at different time (listener on a button that exists in the nav element))
 
-    //function that adds a checklist or note item into a todo and then redraws or adds that checklist or note item as an element
+    //don't forget that functions that add or delete things will need to repaint the page to include/exclude added/deleted items
 
     
 })()
